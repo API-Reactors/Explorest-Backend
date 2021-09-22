7e12a8f63d11d9604d9bbb7d924187994c9c03e9
Yahialabeeb <yahia.labeeb@gmail.com>
2021-09-22 12:07:17 +0300

    final edit1

--- controllers /userIntrests.js
+++ controllers /userIntrests.js
@@ -3,12 +3,12 @@ const User = require("../models/User");
 const { req, res } = require("express");
 
 const putIntrests = (req, res) => {
-  console.log(req.body);
+
   const arrayIntrests = req.body;
   const userId = req.params.user_id;
-  User.findOne({ _id: userId }, (error, userIntrests) => {  
+  User.findOne({ _id: userId }, (error, userIntrests) => {
     userIntrests.intrests = arrayIntrests;
-    console.log(userIntrests.intrests);
+
     userIntrests.save();
     res.json(userIntrests);
   });
